49c35b30c3a2109e9e56940c030ce97ac477a15c
Hyunsik <gomting@naver.com>
2018-04-16 23:10:48 +0900

    ㅁㅁㅁ

--- mongodb.py
+++ mongodb.py
@@ -2,14 +2,21 @@ import console
 from pymongo import MongoClient
 import configparser
 import json
-
+from datetime import datetime
 
 console.clear()
 
-with open('config.json', 'r') as f:
-	config = json.load(f)
+today = datetime.today().strftime("%Y%m%d")
+json_name = 'config.json'
 
-def m_db_init():
+def import_json(json_name):
+	with open(json_name, 'r') as f:
+		config = json.load(f)
+	
+	return config
+
+
+def m_db_init(config, coll):
 	MONGO_HOST = config['mlab']['MONGO_HOST']
 	MONGO_PORT = config['mlab']['MONGO_PORT']
 	MONGO_DB = config['mlab']['MONGO_DB']
@@ -19,13 +26,26 @@ def m_db_init():
 	connection = MongoClient(MONGO_HOST, MONGO_PORT)
 	db = connection[MONGO_DB]
 	check = db.authenticate(MONGO_USER, MONGO_PASS)
-	collection = db['test_KBO']
+	collection = db[coll]
 	
 	return collection
-
-collection = m_db_init()
+	
+	
+config = import_json(json_name)
+coll = 'test_P2P_Assets'
+coll = 'test_P2P_Lists'
+#coll = 'test_KBO'
+collection = m_db_init(config, coll)
 a = collection.find({})
-a = collection.find({'DATE':'20180415'})
+#a = collection.find({'DATE':today})
 for item in a:
-	#print('{0} {1} {2:.1f}%'.format(item['업체명'], item['상품명'], float(item['모집률'])))
-	print(item['NO'], item['TEAM'], item['R'], item['STATUS'])
+	print('{0} {1} {2:.1f}%'.format(item['업체명'], item['상품명'], float(item['모집률'])))
+	#print(item['NO'], item['TEAM'], item['R'], item['STATUS'])
+	i = 0
+	fmt = []
+	for key, value in item.items():
+		if key != '_id':
+			fmt.append('{' + str(i) + '}')
+			i = i + 1
+			#print(key, value if isinstance(value, (int, float, complex)) else value)
+	print(' '.join(fmt).format())
